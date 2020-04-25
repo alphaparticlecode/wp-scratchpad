dae619c34b0d9d5e2c6e543d457b8b26c2f28a02
Keanan Koppenhaver <keanan@alphaparticle.com>
2020-04-25 03:56:20 -0500

    Make site component visibility conditional on whether a URL is already set

--- src/Site.js
+++ src/Site.js
@@ -2,47 +2,50 @@ import React, { useState } from 'react';
 import './Site.css';
 
 function Site() {
-  var trimmedUrl = localStorage.getItem('scratchpadSiteURL') || '';
+	var trimmedUrl = localStorage.getItem('scratchpadSiteURL') || '';
 
-  if( '' !== trimmedUrl ) {
-  	trimmedUrl = trimmedUrl.split('/wp-json')[0];
-  }
-
-  const [siteURL, setURL] = useState(trimmedUrl);
-  const [siteFormVisible, setSiteVisibility] = useState(true);
-
-  function saveSite(event){
-	// Append wp-json OAuth path to the given URL, and if there is no REST API Oauth path at that URL, return an error
-	if( siteURL[siteURL.length - 1] !== '/' ) {
-		var slashedSiteURL = siteURL + '/';
+	if( '' !== trimmedUrl ) {
+		trimmedUrl = trimmedUrl.split('/wp-json')[0];
+		var componentVisible = false;
 	}
 	else {
-		slashedSiteURL = siteURL;
+		componentVisible = true;
 	}
 
-	let url = `${slashedSiteURL}wp-json/`;
+	const [siteURL, setURL] = useState(trimmedUrl);
+  	const [siteFormVisible, setSiteVisibility] = useState(componentVisible);
 
-	// TO-DO: Make sure this is a valid URL and that an OAuth REST API response exists
+	function saveSite(event){
+		if( siteURL[siteURL.length - 1] !== '/' ) {
+			var slashedSiteURL = siteURL + '/';
+		}
+		else {
+			slashedSiteURL = siteURL;
+		}
 
-	// Save the site URL to local storage
-	localStorage.setItem('scratchpadSiteURL', url);
+		let url = `${slashedSiteURL}wp-json/`;
 
-	// Fade out the component
-	setSiteVisibility(false);
+		// TO-DO: Make sure this is a valid URL and that an OAuth REST API response exists
 
-	event.preventDefault();
-  }
+		// Save the site URL to local storage
+		localStorage.setItem('scratchpadSiteURL', url);
 
-  return (
-	<div className={`site ${siteFormVisible ? '' : 'hidden'}`}>
-		<form action="" onSubmit={saveSite}>
-			<label htmlFor="siteURL">Site URL</label>
-			<input type="url" name="siteURL" placeholder="https://example.com" value={siteURL} onChange={event => setURL(event.target.value)}/>
+		// Fade out the component
+		setSiteVisibility(false);
 
-			<input type="submit" value="Save Site"/>
-		</form>
-	</div>
-  );
+		event.preventDefault();
+	}
+
+	return (
+		<div className={`site ${siteFormVisible ? '' : 'hidden'}`}>
+			<form action="" onSubmit={saveSite}>
+				<label htmlFor="siteURL">Site URL</label>
+				<input type="url" name="siteURL" placeholder="https://example.com" value={siteURL} onChange={event => setURL(event.target.value)}/>
+
+				<input type="submit" value="Save Site"/>
+			</form>
+		</div>
+	);
 }
 
 export default Site;
