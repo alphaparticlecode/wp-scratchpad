b2001dc8417784e8ab3ec6edab683c799113dc69
Keanan Koppenhaver <keanan@alphaparticle.com>
2020-04-25 21:27:41 -0500

    Working version of login component

--- src/Login.js
+++ src/Login.js
@@ -1,16 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Login.css';
 
 function Login() {
 	const [hasErrors, setErrors] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
-	const [loginFormVisible, setLoginVisibility] = useState(true);
+
+	const [loginFormVisible, setLoginVisibility] = useState(false);
 
 	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
+	const [api_key, setAPIKey] = useState('');
+	const [api_secret, setAPISecret] = useState('');
+
+	useEffect(function(){
+		validateOrRefreshJWT();
+	}, []);
 
 	function saveLogin(event) {
-		setLoginVisibility(false);
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				api_key: api_key,
+				api_secret: api_secret
+			})
+		};
+
+		var tokenUrl = localStorage.getItem('scratchpadSiteURL') + 'wp/v2/token';
+
+		fetch(tokenUrl, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+			if( data.data.status ) {
+				setErrorMessage('Your credentials were invalid. Please double check your API key and API secret and try again.');	
+
+		    	setErrors(true);
+			}
+			else {
+				setUsername(data.data.user.user_login);
+				localStorage.setItem('username', data.data.user.user_login);
+
+				localStorage.setItem('scratchpadJWT', data.access_token);
+				localStorage.setItem('scratchpadJWTRefresh', data.refresh_token);
+
+				setLoginVisibility(false);
+			}
+        });
 
 		event.preventDefault();
 	}
@@ -19,20 +53,70 @@ function Login() {
 		setLoginVisibility(true);
 	}
 
+	function validateOrRefreshJWT() {
+		if( localStorage.getItem('scratchpadJWT') ) {
+			var validateUrl = localStorage.getItem('scratchpadSiteURL') + 'wp/v2/token/validate';
+
+			const requestOptions = {
+				headers: new Headers({
+			    	'Authorization': 'Bearer ' + localStorage.getItem('scratchpadJWT'), 
+				}), 
+			}
+
+			fetch(validateUrl, requestOptions)
+			.then(response => response.json())
+	        .then(data => {
+				if( data.code !== 'rest_authentication_valid_access_token' ){
+					const requestOptions = {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							refresh_token: localStorage.getItem('scratchpadJWTRefresh'),
+						})
+					};
+
+					var tokenUrl = localStorage.getItem('scratchpadSiteURL') + 'wp/v2/token';
+					fetch(tokenUrl, requestOptions)
+					.then(response => response.json())
+			        .then(data => {
+						if( data.data.status ) {
+							setErrorMessage('Your credentials were invalid. Please double check your API key and API secret and try again.');	
+
+					    	setErrors(true);
+						}
+						else {
+							setUsername(data.data.user.user_login);
+							localStorage.setItem('username', data.data.user.user_login);
+
+							localStorage.setItem('scratchpadJWT', data.access_token);
+							localStorage.setItem('scratchpadJWTRefresh', data.refresh_token);
+
+							setLoginVisibility(false);
+						}
+			        });
+				}
+				else {
+					setUsername(localStorage.getItem('username'));
+					setLoginVisibility(false);
+				}
+	        });
+		}
+	}
+
 	return (
 		<div className="login">
 			<div className={`${loginFormVisible ? 'hidden' : ''}`}>
-				<p className="currentuser">Current User: kkoppenhaver{username}</p>
+				<p className="currentuser">Current User: {username}</p>
 				<button onClick={loginVisible}>Edit Login</button>
 			</div>
 			
 			<form action="" onSubmit={saveLogin} className={`${loginFormVisible ? '' : 'hidden'}`}>
 				<p className={`errors ${hasErrors ? '' : 'hidden'}`}>{errorMessage}</p>
-				<label htmlFor="username">WordPress Username</label>
-				<input type="text" name="username" value={username} onChange={event => setUsername(event.target.value)}/>
+				<label htmlFor="api_key">API Key</label>
+				<input type="text" name="api_key" value={api_key} onChange={event => setAPIKey(event.target.value)}/>
 
-				<label htmlFor="password">WordPress Password</label>
-				<input type="password" name="password" value={password} onChange={event => setPassword(event.target.value)}/>
+				<label htmlFor="api_secret">API Secret</label>
+				<input type="text" name="api_secret" value={api_secret} onChange={event => setAPISecret(event.target.value)}/>
 
 				<input type="submit" value="Log In"/>
 			</form>
